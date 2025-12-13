f6e828668cc505ba0df627aebb6d1de9c726d9d1
Mr_Dwj <explorer-dong@foxmail.com>
2025-12-13 16:08:52 +0800

    fix: invalid github action folder name

--- deploy.sh
+++
@@ -1,6 +0,0 @@
-git push
-
-mkdocs build
-ossutil rm -r oss://wiki-web-shanghai/ -f
-ossutil cp oss://public-assets-shanghai/files/BingSiteAuth.xml oss://wiki-web-shanghai/
-ossutil cp -r ./site/ oss://wiki-web-shanghai/
